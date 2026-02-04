67737876a8c5dec0cb31bf63847314ffc5b9bf42
Ricardo Santos <ricardomcsantos7@gmail.com>
2026-02-04 21:49:16 +0100

    Update convertMarkdown function with bold and italic

--- markdown-to-html-converter/script.js
+++ markdown-to-html-converter/script.js
@@ -5,14 +5,18 @@
 function convertMarkdown() {
   let markdown = document.getElementById("markdown-input").value;
 
-  // Headings level 3
+  // Headings
   markdown = markdown.replace(/^### (.*)$/gm, "<h3>$1</h3>");
-
-  // Headings level 2
   markdown = markdown.replace(/^## (.*)$/gm, "<h2>$1</h2>");
-
-  // Headings level 1
   markdown = markdown.replace(/^# (.*)$/gm, "<h1>$1</h1>");
+
+  // Bold (**text** or __text__)
+  markdown = markdown.replace(/\*\*(.+?)\*\*/g, "<strong>$1</strong>");
+  markdown = markdown.replace(/__(.+?)__/g, "<strong>$1</strong>");
+
+  // Italic (*text* or _text_)
+  markdown = markdown.replace(/\*(.+?)\*/g, "<em>$1</em>");
+  markdown = markdown.replace(/_(.+?)_/g, "<em>$1</em>");
 
   return markdown;
 }
