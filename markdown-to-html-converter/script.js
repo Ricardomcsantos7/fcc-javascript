60cd872a9e5b07df82b2610cfd91d5e7e83603aa
Ricardo Santos <ricardomcsantos7@gmail.com>
2026-02-04 15:03:33 +0100

    Convert markdown headings (h1, h2, h3)

--- markdown-to-html-converter/script.js
+++ markdown-to-html-converter/script.js
@@ -3,10 +3,21 @@
 }
  */
 function convertMarkdown() {
-  const markdown = document.getElementById("markdown-input").value;
+  let markdown = document.getElementById("markdown-input").value;
+
+  // Headings level 3
+  markdown = markdown.replace(/^### (.*)$/gm, "<h3>$1</h3>");
+
+  // Headings level 2
+  markdown = markdown.replace(/^## (.*)$/gm, "<h2>$1</h2>");
+
+  // Headings level 1
+  markdown = markdown.replace(/^# (.*)$/gm, "<h1>$1</h1>");
+
   return markdown;
 }
 
+/* eventListener for user Input */
 const markdownInput = document.getElementById("markdown-input");
 
 markdownInput.addEventListener("input", () => {
