d9e5482c6867a0d624fa6fd18e1c899b3ef98e31
Ricardo Santos <ricardomcsantos7@gmail.com>
2026-02-11 23:29:57 +0100

    Add keybord support

--- drum-machine/script.js
+++ drum-machine/script.js
@@ -1,13 +1,26 @@
 const display = document.getElementById("display");
 const drumPads = document.querySelectorAll(".drum-pad");
 
+function playSound(key) {
+  const audio = document.getElementById(key);
+  if (!audio) return;
+
+  const pad = audio.parentElement;
+
+  audio.currentTime = 0; // allows rapid re-trigger
+  audio.play();
+
+  display.textContent = pad.id;
+}
+
+/* Click */
 drumPads.forEach((pad) => {
   pad.addEventListener("click", () => {
-    const audio = pad.querySelector("audio");
-
-    audio.currentTime = 0; // allows rapid re-trigger
-    audio.play();
-
-    display.textContent = pad.id;
+    playSound(pad.textContent.trim());
   });
+});
+
+/* Keybord */
+document.addEventListener("keydown", (event) => {
+  playSound(event.key.toUpperCase());
 });
